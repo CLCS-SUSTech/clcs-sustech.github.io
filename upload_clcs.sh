c5d418d8564de4c13b494b21e88f96420786419c
xy <innerfirexy@gmail.com>
2019-11-28 11:34:29 -0800

    more useful script

--- upload_clcs.sh
+++ upload_clcs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-scp -r index.html pubs.html google*.html pubs js img fonts files css clcs@rohancp.sdsu.edu:/home/clcs/public_html
+if [[ $# -eq 0 ]]; then
+	echo "Uploading everything ..."
+	scp -r index.html pubs.html google*.html pubs js img fonts files css clcs@rohancp.sdsu.edu:/home/clcs/public_html
+else
+	for var in $@
+	do 
+		echo "Uploading $var ..."
+		scp -r $var clcs@rohancp.sdsu.edu:/home/clcs/public_html"/$var"
+	done
+fi
